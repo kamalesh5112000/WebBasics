aa87bd2bb10248c6f347f066c14e78190f40bd0a
kamalesh5112000 <kamalesh5112000@pec.edu>
2023-05-23 14:08:36 +0530

    dom commit

--- dom.js
+++ dom.js
@@ -1,13 +1,20 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+
+var filter =document.getElementById('filter');
+
+
 form.addEventListener('submit',additem);
 itemList.addEventListener('click',delitem);
+filter.addEventListener('keyup',filteritems);
 function additem(e){
     e.preventDefault();
     var newitem = document.getElementById('item');
+    var itemdis =document.getElementById('description');
     var li=document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(newitem.value));
+    li.appendChild(document.createTextNode(itemdis.value));
     var del=document.createElement('button');
 
     del.className='btn btn-danger btn-sm float-right delete';
@@ -26,4 +33,24 @@ function delitem(e){
             itemList.removeChild(li)
         }
     }
+}
+
+function filteritems(e){
+    var text=e.target.value.toLowerCase();
+    var items= itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        var itemdiscr=item.childNodes;
+        var sec=item.childNodes[1].textContent;
+             
+        //console.log(itemdiscr)
+        console.log(sec)
+        
+         if((itemName.toLowerCase().indexOf(text) != -1)||(sec.toLowerCase().indexOf(text) != -1)){
+            item.style.display = 'block';
+          } else { 
+           item.style.display = 'none';
+         }
+      });
+
 }
