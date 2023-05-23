bece139bb4ea6fe7cc9270afc6d793cb021562f3
kamalesh5112000 <kamalesh5112000@pec.edu>
2023-05-23 12:53:03 +0530

    dom commit

--- dom.js
+++ dom.js
@@ -1,6 +1,29 @@
-// console.dir(document);
-let items=document.getElementsByClassName('list-group-item');
-items[2].style.color='blue';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+form.addEventListener('submit',additem);
+itemList.addEventListener('click',delitem);
+function additem(e){
+    e.preventDefault();
+    var newitem = document.getElementById('item');
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newitem.value));
+    var del=document.createElement('button');
+
+    del.className='btn btn-danger btn-sm float-right delete';
+    del.appendChild(document.createTextNode('X'));
+
+    li.appendChild(del)
+
+    itemList.appendChild(li);
+
+}
+function delitem(e){
+    
+    if(e.target.classList.contains('delete')){
+        if(confirm("Are you Sure?")){
+            var li=e.target.parentElement;
+            itemList.removeChild(li)
+        }
+    }
 }
